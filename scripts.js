32e5c0540bbb1213b0d83b5bc04db0159be8b6eb
Thanh Tran <tltran1@gmail.com>
2015-04-13 23:13:58 -0700

    Add popup.js

---
+++ scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+   $(".group2, .group3, .group4, .group5, .group6, .group7").hide();
+
+   $(".b-close").click(function(){
+        $(".group2").show();
+        $("#my-button").hide();
+    });
+
+});
+
+
+
+// Semicolon (;) to ensure closing of earlier scripting
+// Encapsulation
+// $ is assigned to jQuery
